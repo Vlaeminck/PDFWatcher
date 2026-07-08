be0fa12d4eb7eee6aa1552a00e6707d6cbea4ce0
Vlaeminck <tomasvlaeminck.info@gmail.com>
2026-07-08 15:07:46 -0300

    Agregado start.bat

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ===================================================
+echo Iniciando PDFWatcher (Codigo Abierto)
+echo ===================================================
+python app.py
+pause
